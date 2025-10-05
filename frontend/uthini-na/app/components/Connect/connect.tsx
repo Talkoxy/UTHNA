a69c609761b616a6f530ec110252a2d97ded2d0d
Talkoxy <77780345+Talkoxy@users.noreply.github.com>
2025-10-05 16:47:30 +0200

    error

--- frontend/uthini-na/app/components/Connect/connect.tsx
+++ frontend/uthini-na/app/components/Connect/connect.tsx
@@ -6,7 +6,6 @@ const Connect = () => {
     return(
         <main className="grid place-items-center fixed inset-0">
             <CreateConnectPostButton/>
-            <ConnectPosts/>
         </main>
     )
 }
