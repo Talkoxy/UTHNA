b1f2713b5635a6436e53dcbabdd502385af7ebf0
Talkoxy <77780345+Talkoxy@users.noreply.github.com>
2025-10-05 16:38:52 +0200

    error

--- frontend/uthini-na/app/lib/actions.ts
+++ frontend/uthini-na/app/lib/actions.ts
@@ -3,36 +3,25 @@
 import { cookies } from "next/headers";
 
 export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
-    const ACCESS_TOKEN_MAX_AGE = 60 * 60; // 1 hour (Django setting: 60 minutes)
-    
-    const REFRESH_TOKEN_MAX_AGE = 60 * 60 * 24 * 2; // 2 days (Django setting: 2 days)
-
-
-    // 1. session_userid: User ID cookie (Should match Refresh Token life)
     (await cookies()).set('session_userid', userId, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        maxAge: REFRESH_TOKEN_MAX_AGE, // 2 days
-        path: '/',
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV == 'production', 
+        maxAge: 60 * 60 * 24 * 7, // 2 days
+        path: '/'
     });
 
-    // 2. session_access_token: Access Token cookie
     (await cookies()).set('session_access_token', accessToken,{
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        maxAge: ACCESS_TOKEN_MAX_AGE, // 1 hour (60 minutes)
-        path: '/',
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV == 'production', 
+        maxAge: 60 * 60, // 30 mins 
+        path: '/'
     });
 
-    // 3. session_refresh_token: Refresh Token cookie
     (await cookies()).set('session_refresh_token', refreshToken,{
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production', 
-        maxAge: REFRESH_TOKEN_MAX_AGE, // 2 days
-        path: '/',
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV == 'production', 
+        maxAge: 60 * 60 * 24 * 7, // 2 days 
+        path: '/'
     })
 
 }
--- frontend/uthini-na/app/upgrade/page.tsx
+++
@@ -1,9 +0,0 @@
-import Upgrade from "../components/Upgrade/upgrade";
-
-export default function TranslatePage() {
-  return (
-    <main className="page_bg">
-      <Upgrade/>
-    </main>
-  );
-}
