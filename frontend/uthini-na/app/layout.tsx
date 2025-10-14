3f68d9a153a0d12775391f90fccb9aae8d5c86c1
Talkoxy <77780345+Talkoxy@users.noreply.github.com>
2025-10-14 23:58:35 +0200

    Update layout.tsx

--- frontend/uthini-na/app/layout.tsx
+++ frontend/uthini-na/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-
+import './globals.css'
 import  Navbar from "./components/Navbar/navbar";
 import CreateFeedbackModal from "./components/Modals/CreateFeedbackModal";
 import CreateSettingModal from "./components/Modals/CreateSettingsModal";
