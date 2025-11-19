5fa78d885226432bc4bd73acdceecaf180178b68
Talkoxy <77780345+Talkoxy@users.noreply.github.com>
2025-11-19 14:54:37 +0200

    Update settingsprod.py

--- backend/djangouthinina/djangouthinina/settingsprod.py
+++ backend/djangouthinina/djangouthinina/settingsprod.py
@@ -221,6 +221,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
